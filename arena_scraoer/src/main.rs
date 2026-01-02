44393e4018fca28fd3d341e72b799ad8969b34b7
Jeremy <jeremyvillalva66@gmail.com>
2026-01-02 12:13:14 -0500

    now successfully scrapes the data however it only grabs the first two oh well well fix it eventually

--- arena_scraoer/src/main.rs
+++ arena_scraoer/src/main.rs
@@ -1,7 +1,12 @@
+use std::time::Duration;
+
+use headless_chrome::browser::tab::element;
+
 #[derive(Debug)]
 struct Block {
     url: String,
     image: String,
+    caption: String
 }
 
 fn main() {
@@ -11,12 +16,14 @@ fn main() {
     let browser = headless_chrome::Browser::default().unwrap();
     let tab = browser.new_tab().unwrap();
     tab.navigate_to("https://www.are.na/andreia-de-matos/food-illustration-dszkfpll53g").unwrap();
-    let html_blocks = tab.wait_for_elements("div.virtuoso-grid-item").unwrap();
+    let html_blocks = tab.wait_for_elements("div.virtuoso-grid-item").unwrap(); 
 
-    std::fs::write("screenshot.png", &screenshot_data).unwrap();
+    
+
 
     
     for html_block in html_blocks {
+
         let url = html_block
             .wait_for_element("a")
             .unwrap()
@@ -28,24 +35,25 @@ fn main() {
             .to_owned();
 
         let image = html_block
-            .wait_for_element("img")
+            .wait_for_element_with_custom_timeout("img", Duration::from_secs(15))
             .unwrap()
             .get_attributes()
             .unwrap()
             .unwrap()
-            .get(5)
+            .get(1)
             .unwrap()
             .to_owned();
         
-        // let caption = html_block
-        //     .wait_for_element("div.c-lgwVNZ c-dHTEdC c-lgwVNZ-iepcqn-ellipsis-true c-lgwVNZ-gyaQWK-size-xs c-lgwVNZ-ijkKOHM-css")
-        //     .unwrap()
-        //     .get_inner_text()
-        //     .unwrap();
+        let caption = html_block
+            .wait_for_element("div")
+            .unwrap()
+            .get_inner_text()
+            .unwrap();
 
         let block = Block {
             url,
             image,
+            caption,
         };
         
         blocks.push(block);
@@ -57,14 +65,15 @@ fn main() {
     let mut writer = csv::Writer::from_path(path).unwrap();
 
     writer
-        .write_record(&["url","image"])
+        .write_record(&["url","image", "caption"])
         .unwrap();
 
     for block in blocks {
         let url = block.url;
         let image = block.image;
+        let caption = block.caption;
 
-        writer.write_record(&[url, image]).unwrap();
+        writer.write_record(&[url, image, caption]).unwrap();
     }
 
     writer.flush().unwrap();
